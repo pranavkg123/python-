e80656d59be77b0f151f3b3b7a741b79ffa3c8f4
pranavkg123 <75836381+pranavkg123@users.noreply.github.com>
2024-12-25 22:58:48 +0530

    matrix multiplication using numpy

---
+++ matrix multiplication.py
@@ -0,0 +1,11 @@
+
+import numpy as np
+
+# Define matrices
+A = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+B = np.array([[9, 8, 7], [6, 5, 4], [3, 2, 1]])
+
+# Matrix multiplication
+result = np.dot(A, B)
+print("Resultant Matrix:")
+print(result)
