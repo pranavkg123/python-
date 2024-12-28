c94b3036178f3e791cf5d32153374e775dfc8fc8
pranavkg123 <75836381+pranavkg123@users.noreply.github.com>
2024-12-28 22:20:14 +0530

    python code for basic  Statistics

---
+++ basic satistics.py
@@ -0,0 +1,18 @@
+import numpy as np
+data = np.array([1, 2, 3, 4, 5])
+
+# Mean
+mean_value = np.mean(data)
+print("Mean:", mean_value)
+
+# Median
+median_value = np.median(data)
+print("Median:", median_value)
+
+# Standard deviation
+std_dev = np.std(data)
+print("Standard Deviation:", std_dev)
+
+# Sum
+sum_value = np.sum(data)
+print("Sum:", sum_value)
